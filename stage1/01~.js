2e8946629aab509b464aa1416f99d905c70bc5b4
riverhye <uhzigani@gmail.com>
2023-11-01 03:14:48 +0900

    백준 1000 시도중

---
+++ stage1/01~.js
@@ -0,0 +1,10 @@
+// 2557
+console.log("Hello World!");
+
+// 1000
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// const A = parseInt(input[0]);
+// const B = parseInt(input[1]);
+
+// console.log(A+B);
