1eac47d47e4673a464849830f3d190ed2d1ff7c3
riverhye <uhzigani@gmail.com>
2023-11-02 01:05:45 +0900

    백준 1001, 10998

--- stage1/01~.js
+++ stage1/01~.js
@@ -1,9 +1,10 @@
 // 2557
-console.log("Hello World!");
+// console.log("Hello World!");
 
-// 1000
-const fs = require("fs");
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// 1000, 1001, 10998 (+, -, *)
+// const fs = require("fs");
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
 // const A = parseInt(input[0]);
 // const B = parseInt(input[1]);
 
