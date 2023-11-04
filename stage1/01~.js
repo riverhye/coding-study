eddb148ae552fa0a4125259a0e4c7774a0938c32
riverhye <uhzigani@gmail.com>
2023-11-04 09:29:17 +0900

    10869 풀이

--- stage1/01~.js
+++ stage1/01~.js
@@ -9,3 +9,11 @@
 // const B = parseInt(input[1]);
 
 // console.log(A+B);
+
+// 10869
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+console.log((A+B) + '\n' + (A-B) + '\n' + (A*B) + '\n' + (Math.floor(A/B)) + '\n' + (A%B));
