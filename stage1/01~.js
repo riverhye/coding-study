32a1f8c2dd572374570bb1bc8b52c0fe99946dbd
riverhye <uhzigani@gmail.com>
2023-11-07 00:52:07 +0900

    2588 풀이

--- stage1/01~.js
+++ stage1/01~.js
@@ -11,9 +11,19 @@
 // console.log(A+B);
 
 // 10869
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
-const A = Number(input[0]);
-const B = Number(input[1]);
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// const A = Number(input[0]);
+// const B = Number(input[1]);
 
-console.log((A+B) + '\n' + (A-B) + '\n' + (A*B) + '\n' + (Math.floor(A/B)) + '\n' + (A%B));
+// console.log((A+B) + '\n' + (A-B) + '\n' + (A*B) + '\n' + (Math.floor(A/B)) + '\n' + (A%B));
+
+// 2588
+// const input = ['472', '385']
+// const a = Number(input[0]);
+// const b = Number(input[1]);
+// const arrB = input[1].split('').reverse();
+// console.log(arrB)
+
+// arrB.map((val)=>console.log(a*val))
+// console.log(a*b);
