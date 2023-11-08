97b6ebc195338586540a6b1d6ed23f2f639667db
riverhye <uhzigani@gmail.com>
2023-11-08 15:35:08 +0900

    10171, 10172 Done

---
+++ stage1/10171~2.js
@@ -0,0 +1,15 @@
+// const [cat] = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`)
+
+// 이스케이프 문 : 백슬래시, 작은따옴표
+
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|
+`)
