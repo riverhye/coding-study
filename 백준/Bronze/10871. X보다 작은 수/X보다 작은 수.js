13f328bd2a09ae932829244d6569a5c886ab8557
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-05 20:22:37 +0900

    [Bronze V] Title: X보다 작은 수, Time: 152 ms, Memory: 11996 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
+const [n, x] = input[0].split(' ');
+const numArr = input[1].split(' ');
+
+const newArr = numArr.filter(num => +x > +num).toString().replaceAll(',', ' ');
+
+console.log(newArr);
