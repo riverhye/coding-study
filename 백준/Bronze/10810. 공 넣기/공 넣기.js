8ccf3b97698beeda70dce28e33de42160e443e69
riverhye <uhzigani@gmail.com>
2024-04-07 19:36:05 +0900

    💡 주석 추가

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -1,26 +1,37 @@
-const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
+// bucket에는 n개의 요소가 있고, 인덱스는 0부터 시작해 n-1까지다.
+// 주머니 넣기(for문 순회)는 총 m회 실시된다.
+
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
 const arr = input[0].split(' ').map(Number);
 const n = arr[0];
 const m = arr[1];
 
+// 바구니 생성 : n개의 초기값
 let bucket = [];
-for(let i = 0; i < n; i++) {
-	bucket.push(0);
+for (let i = 0; i < n; i++) {
+  bucket.push(0);
 }
 
-for(let i = 1; i <= m; i++) { // cmd에 대한 index : i
-	let cmd = input[i].split(' ').map(Number);
-	let startIdx = cmd[0];
-	let endIdx = cmd[1];
-	let num = cmd[2];
+for (let i = 1; i <= m; i++) {
+  let cmd = input[i].split(' ').map(Number);
 
-		bucket[startIdx - 1] = num;
-		bucket[endIdx - 1] = num;
-		if(endIdx - startIdx > 0) {
-			for(let i=startIdx; i<endIdx; i++) {
-				bucket[i - 1] = num;
-			}
-		}
+  let startIdx = cmd[0];
+  let endIdx = cmd[1];
+  let num = cmd[2];
+
+  bucket[startIdx - 1] = num;
+  bucket[endIdx - 1] = num;
+
+  // 둘 이상의 바구니에 넣을 경우
+  if (endIdx - startIdx > 0) {
+    for (let i = startIdx; i < endIdx; i++) {
+      bucket[i - 1] = num;
+    }
+  }
 }
 
 console.log(bucket.join(' '));
