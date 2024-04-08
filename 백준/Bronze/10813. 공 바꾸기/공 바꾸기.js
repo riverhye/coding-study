eac43ecd5dd7b3d2e7df9c60fa4601c2ea076d7b
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-08 15:22:44 +0900

    공 바꾸기.js Ver2.

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
@@ -1,3 +1,4 @@
+// Ver 1.
 const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 
@@ -14,5 +15,21 @@ for(let idx = 1; idx <= m; idx++) {
 	bucket = copyBucket;
 }
 
+console.log(bucket.slice(1).join(' '));
+
+// Ver 2.
+const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+
+// make a bucket
+const bucket = Array(n + 1).fill(0).map((el, i) => el + i);
+
+// 배열 전체 복사하는 대신 한 값만 변수에 담아두기
+for(let idx = 1; idx <= m; idx++) {
+	const [i, j] = input[idx].split(' ');
+	const tmp = bucket[j];
+	bucket[j] = bucket[i];
+	bucket[i] = tmp;
+}
 
 console.log(bucket.slice(1).join(' '));
