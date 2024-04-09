7c67d274070cf1dc12c0d68b616502d68e64a6e2
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-09 12:48:05 +0900

    다른 풀이 추가

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -1,3 +1,18 @@
+// Ver 1. 배열 만들어 메서드 활용
+const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n').map(Number);
+
+let result = [];
+for(let i = 1; i <= 30; i++) {
+	if(input.includes(i)) continue;
+	result.push(i);
+}
+
+result.sort((a, b) => a - b); // 오름차순 정렬
+
+console.log(result.join('\n'));
+
+
+// Ver 2. 배열 생성 없이 순회문의 인덱스 이용
 const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n').map(Number);
 
 let result = [];
