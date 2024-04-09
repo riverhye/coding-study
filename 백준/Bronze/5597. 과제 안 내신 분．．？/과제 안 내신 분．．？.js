00e2e742b5cfd80e15f3b0462e7cebe27531fb49
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-09 12:48:50 +0900

    Update 다른 풀이 추가

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -1,10 +1,12 @@
 // Ver 1. 배열 만들어 메서드 활용
 const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n').map(Number);
 
+
+const arr = Array(30).fill(1).map((a,b) => a+b); //1부터 30이 담긴 배열
+
 let result = [];
-for(let i = 1; i <= 30; i++) {
-	if(input.includes(i)) continue;
-	result.push(i);
+for(let i = 0; i < 30; i++) {
+	result = arr.filter(el => !input.includes(el));
 }
 
 result.sort((a, b) => a - b); // 오름차순 정렬
