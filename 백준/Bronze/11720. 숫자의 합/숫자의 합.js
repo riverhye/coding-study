c18b254b7bbba42f24911568d6464177c55ad37b
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-11 22:49:30 +0900

    [Bronze IV] Title: 숫자의 합, Time: 128 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -4,8 +4,13 @@ const nums = input[1];
 
 const numsArr = nums.split('').map(Number);
 
-const result = numsArr.reduce((acc, cur, idx) => {
-	return acc += cur;
-}, 0)
+// Ver 1. reduce 활용
+const result = numsArr.reduce((acc, cur, idx) => acc += cur);
+
+// Ver 2. for문으로 하기 (메모리 할당 필요)
+// let result = 0;
+// for(let i = 0; i < N; i++) {
+// 	result += numsArr[i];
+// }
 
 console.log(result);
