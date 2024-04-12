a6b35c77e424912e1d98f3f5379615e0df3cac21
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-12 14:01:39 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 136 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -1,17 +1,8 @@
-const S = require('fs').readFileSync("/dev/stdin").toString().trim().split('');
+const S = require('fs').readFileSync("/dev/stdin").toString().trim();
 
-// alphabet 배열
-const alphabet = Array.from({length: 26}, (value, idx) => String.fromCharCode( 97 + idx));
-
-for(let i = 0; i < 26; i++) { // 알파벳 순회
-    for(let j = 0; j < S.length; j++ ) { // S 순회
-    
-        if(S[j].includes(alphabet[i])) {
-            alphabet[i] = S.indexOf(S[j]);
-        }
-    }
+let result = [];
+for(let i = 97; i <= 122; i++) {
+	result.push(S.indexOf(String.fromCharCode(i)));
 }
 
-// string 타입만 -1로 교체
-const newAlphabet = alphabet.join(' ').replaceAll(/[a-z]/g, -1);
-console.log(newAlphabet);
+console.log(result.join(' '));
