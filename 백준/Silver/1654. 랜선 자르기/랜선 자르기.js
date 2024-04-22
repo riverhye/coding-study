d98a74dc618c554d14d5b7974fdb8c27b72f26ce
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-04-22 23:46:47 +0900

    Update 랜선 자르기.js

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -1,29 +1,24 @@
 const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
-// shift로 인덱스 0 분리
 const [K, N] = input.shift().split(' ').map(Number);
-// 나머지 input 정렬
 const lines = input.map(Number).sort((a, b) => a - b);
 
 let min = 1;
 let max = Math.max(...lines);
 
-// 이진 탐색
 while(min <= max) {
-    // mid : 랜선을 자르는 길이 기준
 	const mid = Math.floor((min + max) / 2);
 	
+	// 각 배열 요소를 기준값으로 자르기
 	// <문자열을 숫자로 변환>
 	// Number : 순수하게 숫자만, 숫자와 문자열이 포함될 경우 NaN 반환
 	// parseInt : 숫자로 시작할 시 문자열 전까지만 숫자로, 문자열로 시작하면 NaN 반환, 소수점 없이 정수만
-	const pieceCount = lines.map(line => parseInt(line / mid)).reduce((acc, cur) => acc + cur, 0);
+	const pieceCount = lines.reduce((acc, cur) => parseInt(acc + cur / mid), 0);
 	
-    // 목표 개수보다 작다 => 자르는 기준(mid)이 너무 크다 => 탐색 범위를 좁힌다
-	if (pieceCount < N) { 
-        max = mid - 1;
-    // 목표 개수와 같거나 크다 => 자르는 기준(mid) 중에서 최댓값을 찾자
+	if (pieceCount < N) { // 목표 개수보다 작으면
+		max = mid - 1; // mid를 줄여서 더 작은 기준값으로 자르기
 	} else {
-		min = mid + 1; 
+		min = mid + 1; // 목표 개수와 같거나 클 땐 mid를 키워서 최대값에 가까워지기
 	}
 }
 
-console.log(max);
+console.log(max); // 최대 개수
