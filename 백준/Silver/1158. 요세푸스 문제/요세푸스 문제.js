e2a2e208b10a729041100443c2bc98961c2182ca
Yunhye Park <77149171+riverhye@users.noreply.github.com>
2024-05-10 22:55:40 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2636 ms, Memory: 18072 KB

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -5,11 +5,16 @@ let arr = new Array(N).fill(1).map((a, b) => a + b);
 let result = [];
 let i = 1;
 while(arr.length > 0) {
-    const shiftedEl = arr.shift();
+    // arr를 다 비우고, 제거 순서대로 result 배열에 추가하려는 것
+    // 배열의 첫 요소를 제거
+    const firstEl = arr.shift();
+    // 지금 순서가 제거해야 할 순서의 배수라면
     if(i % K == 0) {
-        result.push(shiftedEl);
+        // 결과 배열에 추가
+        result.push(firstEl);
     } else {
-        arr.push(shiftedEl);
+        // 아니라면, 원 배열 끝에 다시 추가
+        arr.push(firstEl);
     }
     i++;
 }
